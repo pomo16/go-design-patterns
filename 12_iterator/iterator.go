c8d83c5062353759a56f8dc81ee95a44b1f11bee
pomo <m13143378602@163.com>
2020-07-11 16:13:39 +0800

    add iterator

--- 12_iterator/iterator.go
+++ 12_iterator/iterator.go
@@ -4,3 +4,54 @@ package iterator
 	Iterator 迭代器模式
 */
 
+//IAggregate 容器接口
+type IAggregate interface {
+	Iterator() IIterator
+}
+
+//IIterator 迭代器接口
+type IIterator interface {
+	HasNext() bool
+	Current() int
+	Next() bool
+}
+
+//Aggregate 具体容器实现
+type Aggregate struct {
+	container []int // 容器中装载 int 型容器
+}
+
+//Iterator 创建一个迭代器，并让迭代器中的容器指针指向当前对象
+func (a *Aggregate) Iterator() IIterator {
+	i := new(Iterator)
+	i.aggregate = a
+	return i
+}
+
+//Iterator 迭代器实现
+type Iterator struct {
+	cursor    int // 当前游标
+	aggregate *Aggregate // 对应的容器指针
+}
+
+//HasNext 判断是否迭代到最后，如果没有，则返回true
+func (i *Iterator) HasNext() bool {
+	if i.cursor+1 < len(i.aggregate.container) {
+		return true
+	}
+	return false
+}
+
+//Current 获取当前迭代元素（从容器中取出当前游标对应的元素）
+func (i *Iterator) Current() int {
+	return i.aggregate.container[i.cursor]
+}
+
+//Next 将游标指向下一个元素
+func (i *Iterator) Next() bool {
+	if i.cursor < len(i.aggregate.container) {
+		i.cursor++
+		return true
+	}
+	return false
+}
--- 12_iterator/iterator_test.go
+++ 12_iterator/iterator_test.go
@@ -1,7 +1,29 @@
 package iterator
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestIterator(t *testing.T) {
+	//创建容器并初始化
+	c := &Aggregate{
+		container: []int{1, 2, 3, 4},
+	}
+
+	//获取迭代器
+	iterator := c.Iterator()
+
+	for {
+		// 打印当前数据
+		fmt.Println(iterator.Current())
+		// 如果有下一个元素，则将游标移动到下一个元素
+		// 否则跳出循环，迭代结束
+		if iterator.HasNext() {
+			iterator.Next()
+		} else {
+			break
+		}
+	}
 
 }
